d5e084c715c841deee352538a764d4fdf38fcf62
PSNAppZ <thepsnarayanan@gmail.com>
2025-06-27 10:36:02 +0530

    Refactor disbursement controllers to utilize JWT signature validation. Update method signatures to use Annotated for dependency injection, enhancing code clarity and maintainability.

--- openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/disbursement.py
+++ openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/disbursement.py
@@ -44,8 +44,7 @@ class DisbursementController(BaseController):
     async def create_disbursements(
         self,
         disbursement_request: DisbursementRequest,
-        is_signature_valid: bool = True,
-        # is_signature_valid = Annotated[bool, Depends(JWTSignatureValidator())],
+        is_signature_valid=Annotated[bool, Depends(JWTSignatureValidator())],
     ) -> DisbursementResponse:
         _logger.info("Creating disbursements")
         try:
