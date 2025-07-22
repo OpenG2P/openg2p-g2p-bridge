722878b26d11b4c51548f09adc8884b80798321b
PSNAppZ <thepsnarayanan@gmail.com>
2025-07-22 17:18:49 +0530

    Enhance disbursement envelope status tests by updating mock service methods and adding new fields to the DisbursementEnvelopeStatusPayload. Refactor request payload construction for improved clarity and consistency in both success and failure scenarios.

--- openg2p-g2p-bridge-api/tests/test_disbursement_envelope_status.py
+++ openg2p-g2p-bridge-api/tests/test_disbursement_envelope_status.py
@@ -37,10 +37,18 @@ async def test_get_disbursement_envelope_status_success(
     mock_service_get_component.return_value = mock_service_instance
 
     # Mock service methods
-    mock_service_instance.get_disbursement_envelope_batch_status = AsyncMock(
+    mock_service_instance.get_disbursement_envelope_status = AsyncMock(
         return_value=DisbursementEnvelopeStatusPayload(
             disbursement_envelope_id="env123",
+            benefit_code_id=1,
+            benefit_code_mnemonic="BEN123",
+            benefit_type="CASH_DIGITAL",
+            measurement_unit="kg",
+            number_of_beneficiaries_received=100,
+            number_of_beneficiaries_declared=100,
+            number_of_disbursements_declared=100,
             number_of_disbursements_received=100,
+            total_disbursement_quantity_declared=5000.0,
             total_disbursement_quantity_received=5000,
             funds_available_with_bank=FundsAvailableWithBankEnum.FUNDS_AVAILABLE,
             funds_available_latest_timestamp=datetime.now(),
@@ -51,38 +59,73 @@ async def test_get_disbursement_envelope_status_success(
             funds_blocked_latest_error_code=None,
             funds_blocked_attempts=2,
             funds_blocked_reference_number="ref123",
-            id_mapper_resolution_required=False,
             number_of_disbursements_shipped=100,
             number_of_disbursements_reconciled=95,
             number_of_disbursements_reversed=5,
+            no_of_warehouses_allocated=1,
+            no_of_warehouses_notified=1,
+            no_of_agencies_allocated=1,
+            no_of_agencies_notified=1,
+            no_of_beneficiaries_notified=100,
+            no_of_pods_received=None,
+            disbursement_batch_control_geos=None,
         )
+    )
+
+    request_header = RequestHeader(
+        message_id="123",
+        message_ts=datetime.now().isoformat(),
+        action="",
+        sender_id="",
+        sender_uri="",
+        receiver_id="",
+        total_count=1,
+        is_msg_encrypted=False,
+    )
+    request_payload = DisbursementEnvelopeStatusRequest(
+        header=request_header,
+        message="env123",
     )
 
     expected_response = DisbursementEnvelopeStatusResponse(
         header=SyncResponseHeader(
-            message_id="",
-            message_ts=datetime.now().isoformat(),
-            action="",
+            message_id=request_header.message_id,
+            message_ts=request_header.message_ts,
+            action=request_header.action,
             status=StatusEnum.succ,
             status_reason_message="",
         ),
         message=DisbursementEnvelopeStatusPayload(
             disbursement_envelope_id="env123",
+            benefit_code_id=1,
+            benefit_code_mnemonic="BEN123",
+            benefit_type="CASH_DIGITAL",
+            measurement_unit="kg",
+            number_of_beneficiaries_received=100,
+            number_of_beneficiaries_declared=100,
+            number_of_disbursements_declared=100,
             number_of_disbursements_received=100,
+            total_disbursement_quantity_declared=5000.0,
             total_disbursement_quantity_received=5000,
             funds_available_with_bank=FundsAvailableWithBankEnum.FUNDS_AVAILABLE,
-            funds_available_latest_timestamp=datetime.now(),
+            funds_available_latest_timestamp=None,
             funds_available_latest_error_code=None,
             funds_available_attempts=3,
             funds_blocked_with_bank=FundsBlockedWithBankEnum.FUNDS_BLOCK_SUCCESS,
-            funds_blocked_latest_timestamp=datetime.now(),
+            funds_blocked_latest_timestamp=None,
             funds_blocked_latest_error_code=None,
             funds_blocked_attempts=2,
             funds_blocked_reference_number="ref123",
-            id_mapper_resolution_required=False,
             number_of_disbursements_shipped=100,
             number_of_disbursements_reconciled=95,
             number_of_disbursements_reversed=5,
+            no_of_warehouses_allocated=1,
+            no_of_warehouses_notified=1,
+            no_of_agencies_allocated=1,
+            no_of_agencies_notified=1,
+            no_of_beneficiaries_notified=100,
+            no_of_pods_received=None,
+            disbursement_batch_control_geos=None,
         ),
     )
 
@@ -92,20 +135,6 @@ async def test_get_disbursement_envelope_status_success(
 
     # Instantiate controller and make request
     controller = DisbursementEnvelopeStatusController()
-    request_payload = DisbursementEnvelopeStatusRequest(
-        header=RequestHeader(
-            message_id="123",
-            message_ts=datetime.now().isoformat(),
-            action="",
-            sender_id="",
-            sender_uri="",
-            receiver_id="",
-            total_count=1,
-            is_msg_encrypted=False,
-        ),
-        message="env123",
-    )
-
     actual_response = await controller.get_disbursement_envelope_status(
         request_payload, is_signature_valid=True
     )
@@ -129,15 +158,30 @@ async def test_get_disbursement_envelope_status_failure(
     mock_service_get_component.return_value = mock_service_instance
 
     # Mock service methods to raise an error
-    mock_service_instance.get_disbursement_envelope_batch_status.side_effect = (
+    mock_service_instance.get_disbursement_envelope_status.side_effect = (
         DisbursementStatusException(code=error_code, message=f"{error_code} error.")
+    )
+
+    request_header = RequestHeader(
+        message_id="123",
+        message_ts=datetime.now().isoformat(),
+        action="",
+        sender_id="",
+        sender_uri="",
+        receiver_id="",
+        total_count=1,
+        is_msg_encrypted=False,
+    )
+    request_payload = DisbursementEnvelopeStatusRequest(
+        header=request_header,
+        message="env123",
     )
 
     error_response = DisbursementEnvelopeStatusResponse(
         header=SyncResponseHeader(
-            message_id="",
-            message_ts=datetime.now().isoformat(),
-            action="",
+            message_id=request_header.message_id,
+            message_ts=request_header.message_ts,
+            action=request_header.action,
             status=StatusEnum.rjct,
             status_reason_message=error_code,
         ),
@@ -150,20 +194,6 @@ async def test_get_disbursement_envelope_status_failure(
 
     # Instantiate controller and make request
     controller = DisbursementEnvelopeStatusController()
-    request_payload = DisbursementEnvelopeStatusRequest(
-        header=RequestHeader(
-            message_id="123",
-            message_ts=datetime.now().isoformat(),
-            action="",
-            sender_id="",
-            sender_uri="",
-            receiver_id="",
-            total_count=1,
-            is_msg_encrypted=False,
-        ),
-        message="env123",
-    )
-
     actual_response = await controller.get_disbursement_envelope_status(
         request_payload, is_signature_valid=True
     )
