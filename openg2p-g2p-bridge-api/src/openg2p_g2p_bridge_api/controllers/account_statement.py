7c263f4b0087e2d1e60511f17b36e9b9aecff3fc
PSNAppZ <thepsnarayanan@gmail.com>
2025-07-23 12:41:46 +0530

    Remove unused signature validation parameter from upload_mt940 method in AccountStatementController for cleaner code and improved clarity.

--- openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/account_statement.py
+++ openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/account_statement.py
@@ -38,13 +38,10 @@ class AccountStatementController(BaseController):
 
     async def upload_mt940(
         self,
-        # is_signature_valid: Annotated[bool, Depends(JWTSignatureValidator())],
-        is_signature_valid: bool = True,
         statement_file: UploadFile = File(...),
     ) -> AccountStatementResponse:
         _logger.info("Uploading statement file")
         try:
-            RequestValidation.get_component().validate_signature(is_signature_valid)
             RequestValidation.get_component().validate_request(statement_file)
             RequestValidation.get_component().validate_mt940_file(statement_file)
             account_statement_id: str = (
