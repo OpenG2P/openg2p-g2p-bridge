07194361b72a065e20d55e61b418ff83c9af3d2d
PSNAppZ <thepsnarayanan@gmail.com>
2025-09-15 20:35:12 +0530

    Add loggers

--- openg2p-g2p-bridge-celery-workers/src/openg2p_g2p_bridge_celery_workers/tasks/check_funds_with_bank.py
+++ openg2p-g2p-bridge-celery-workers/src/openg2p_g2p_bridge_celery_workers/tasks/check_funds_with_bank.py
@@ -27,7 +27,6 @@ _engine = get_engine()
 @celery_app.task(name="check_funds_with_bank_worker")
 def check_funds_with_bank_worker(disbursement_envelope_id: str):
     _logger.info(f"Checking funds with bank for envelope: {disbursement_envelope_id}")
-    _logger.info(f"DB HOSTNAME: {_config.db_hostname}")
     session_maker = sessionmaker(bind=_engine.get("db_engine_bridge"), expire_on_commit=False)
 
     with session_maker() as session:
@@ -61,6 +60,7 @@ def check_funds_with_bank_worker(disbursement_envelope_id: str):
         )
 
         total_funds_needed = disbursement_envelope.total_disbursement_quantity
+        _logger.info(f"Check funds in bank {sponsor_bank_configuration.sponsor_bank_code} for account {sponsor_bank_configuration.program_account_number} for amount {total_funds_needed} {disbursement_envelope.measurement_unit}")
         bank_connector = BankConnectorFactory.get_component().get_bank_connector(
             sponsor_bank_configuration.sponsor_bank_code
         )
