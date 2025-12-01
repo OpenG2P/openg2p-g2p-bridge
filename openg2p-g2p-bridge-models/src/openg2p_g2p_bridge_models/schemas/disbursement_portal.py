ac6eff2e3733746511941a63ee14a44bb207c1ff
tahzeer <tahzeer.ash@gmail.com>
2025-12-01 09:42:50 +0530

    [G2P-3639]refactor: remove g2p prefix from bridge bene portal api and related schemas

--- openg2p-g2p-bridge-models/src/openg2p_g2p_bridge_models/schemas/disbursement_portal.py
+++ openg2p-g2p-bridge-models/src/openg2p_g2p_bridge_models/schemas/disbursement_portal.py
@@ -31,38 +31,38 @@ class DisbursementSummary(BaseModel):
 
 
 class DisbursementRequestBody(BaseModel):
-    g2p_pagination_request: Optional[G2PPaginationRequest] = None
-    g2p_request_payload: Optional[dict] = None
+    pagination_request: Optional[G2PPaginationRequest] = None
+    request_payload: Optional[dict] = None
 
 
 class DisbursementRequest(BaseModel):
-    g2p_request_header: G2PRequestHeader
-    g2p_request_body: Optional[DisbursementRequestBody] = None
+    request_header: G2PRequestHeader
+    request_body: Optional[DisbursementRequestBody] = None
 
 
 class DisbursementResponse(BaseModel):
-    g2p_response_header: G2PResponseHeader
-    g2p_response_body: "DisbursementResponseBody"
+    response_header: G2PResponseHeader
+    response_body: "DisbursementResponseBody"
 
 
 class DisbursementResponseBody(BaseModel):
-    g2p_pagination_response: Optional[G2PPaginationResponse] = None
-    g2p_response_payload: List[Disbursement]
+    pagination_response: Optional[G2PPaginationResponse] = None
+    response_payload: List[Disbursement]
 
 
 class DisbursementSummaryRequestBody(BaseModel):
-    g2p_request_payload: Optional[dict] = None
+    equest_payload: Optional[dict] = None
 
 
 class DisbursementSummaryRequest(BaseModel):
-    g2p_request_header: G2PRequestHeader
-    g2p_request_body: Optional[DisbursementSummaryRequestBody] = None
+    request_header: G2PRequestHeader
+    request_body: Optional[DisbursementSummaryRequestBody] = None
 
 
 class DisbursementSummaryResponse(BaseModel):
-    g2p_response_header: G2PResponseHeader
-    g2p_response_body: "DisbursementSummaryResponseBody"
+    response_header: G2PResponseHeader
+    response_body: "DisbursementSummaryResponseBody"
 
 
 class DisbursementSummaryResponseBody(BaseModel):
-    g2p_response_payload: List[DisbursementSummary]
+    response_payload: List[DisbursementSummary]
